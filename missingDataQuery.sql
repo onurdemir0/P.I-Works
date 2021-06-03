0711fe4073aa347748053d0824fed0a009b11121
Onur <43968078+onurdemir0@users.noreply.github.com>
2021-06-03 19:10:07 +0300

    Add files via upload

---
+++ missingDataQuery.sql
@@ -0,0 +1,11 @@
+select 
+    c.*,
+    coalesce(
+        c.val, 
+        (
+            select percentile_cont(0.5) within group(order by c1.val)
+            from test c1
+            where c1.id between c.id - 3 and c.id - 1
+        )
+    ) fixed
+from test c
